4f125d610043cadbc25ed6d9fd78d5f94b20f5ad
Keith Koberle <kdkoberle@madisoncollege.edu>
2012-12-02 22:57:34 -0600

    OSS Layout Added

--- app/models/webmark.rb
+++ app/models/webmark.rb
@@ -1,3 +1,8 @@
 class Webmark < ActiveRecord::Base
   attr_accessible :date_saved, :name, :url
+  
+  validates :url, :length => { :minimum => 10 }
+  validates :name, :length => { :maximum => 99 }
+  validates :name, :length => { :minimum => 1 }
+  
 end
