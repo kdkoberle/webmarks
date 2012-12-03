3ee1ec8193ae7c583bb8b879def2a92182f750eb
Keith Koberle <kdkoberle@madisoncollege.edu>
2012-12-02 20:01:45 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webmarks::Application.config.secret_token = 'b2b5bcee08a05a3a63dc01619608ac33d02a9363544d05aa6bf31ab7a7c25defb990dfd6b6bed7803d42519047f4b74abea4d1a057ce14319f8736870c2813d3'
